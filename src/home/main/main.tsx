7e00ee8852f19b1d26c5df139e31f8fc1babaeea
JORGE <jorgenathanael53@gmail.com>
2025-01-24 05:54:19 -0300

    organizing semi-dynamic data

--- src/home/main/main.tsx
+++ src/home/main/main.tsx
@@ -1,118 +1,38 @@
-import "./main.scss"
+"use client";
+import { useEffect } from "react";
+import "./main.scss";
+import products from "./produtos.json";
 
-export default function Main(){
+export default function Main() {
+  const Produtos = products;
 
-    return(
-        <main>
-            <section>
-                <img src="https://i.postimg.cc/ZK6trT0y/Banner-1.png" alt="Banner de promoção" />
+  return (
+    <main>
+      <section>
+        <img
+          src="https://i.postimg.cc/ZK6trT0y/Banner-1.png"
+          alt="Banner de promoção"
+        />
+      </section>
+
+      {Object.entries(Produtos).map(([produto, dados]) => {
+        return (
+          <section key={produto}>
+            <h2>{dados.CatalogTitle}</h2>
+            <section id="cards">
+              {Array.from({ length: 3 }).map((_, index) => (
+                <article key={index}>
+                  <img src={dados.imgProduct} alt="imagem do produto" />
+                  <div>
+                    <h3>{dados.productName}</h3>
+                    <p>{dados.priceProduct}</p>
+                  </div>
+                </article>
+              ))}
             </section>
-
-            <section>
-                <h2>NOTEBOOKS - LAPTOPS</h2>
-                <section id="cards">
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/bNyW2QX4/produto-1.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Notebook Gamer Acer Nitro 5</h3>
-                            <p>R$ 3.479,00</p>
-                        </div>
-                    </article>
-
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/bNyW2QX4/produto-1.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Notebook Gamer Acer Nitro 5</h3>
-                            <p>R$ 3.479,00</p>
-                        </div>
-                    </article>
-
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/bNyW2QX4/produto-1.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Notebook Gamer Acer Nitro 5</h3>
-                            <p>R$ 3.479,00</p>
-                        </div>
-                    </article>
-                </section>
-            </section>
-
-            <section>
-                <h2>FONES DE OUVIDO</h2>
-                <section id="cards">
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/TPgCswMr/produto-2.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Fone de Ouvido Bluetooth</h3>
-                            <p>R$ 74,90</p>
-                        </div>
-                    </article>
-
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/TPgCswMr/produto-2.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Fone de Ouvido Bluetooth</h3>
-                            <p>R$ 74,90</p>
-                        </div>
-                    </article>
-
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/TPgCswMr/produto-2.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Fone de Ouvido Bluetooth</h3>
-                            <p>R$ 74,90</p>
-                        </div>
-                    </article>
-                </section>
-            </section>
-
-            <section>
-                <h2>TECLADOS</h2>
-                <section id="cards">
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/tgJxcR1B/produto-3.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Teclado mecânico gamer</h3>
-                            <p>R$ 119,99</p>
-                        </div>
-                    </article>
-
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/tgJxcR1B/produto-3.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Teclado mecânico gamer</h3>
-                            <p>R$ 119,99</p>
-                        </div>
-                    </article>
-
-                    <article>
-                        
-                            <img src="https://i.postimg.cc/tgJxcR1B/produto-3.png" alt="imagem do produto" />
-                        
-                        <div>
-                            <h3>Teclado mecânico gamer</h3>
-                            <p>R$ 119,99</p>
-                        </div>
-                    </article>
-                </section>
-            </section>
-
-        </main>
-    )
+          </section>
+        );
+      })}
+    </main>
+  );
 }
