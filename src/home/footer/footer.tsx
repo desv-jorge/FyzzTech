2244ff03f83e9fdb09eb801752e2d1960176c43a
JORGE <jorgenathanael53@gmail.com>
2025-01-25 07:39:29 -0300

    organizing responsiveness

--- src/home/footer/footer.tsx
+++ src/home/footer/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer (){
 
         <footer>
             <h2>FYZZTECH©</h2>
-            <p>email de contato: fyzztech@gmail.com</p>
+            <p>fyzztech@gmail.com</p>
             <div>
                 <a href=""> <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn" id="Linkedin"/> </a>
                 <a href=""> <img src="https://i.postimg.cc/Jh94rRn7/Logo-Insta.png" alt="Instagram" /> </a>
