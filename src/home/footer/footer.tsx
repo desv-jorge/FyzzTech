7cb2d032582bd87ad7bda2120188fedafdf3836c
JORGE <jorgenathanael53@gmail.com>
2025-01-25 23:18:27 -0300

    organizing footer

--- src/home/footer/footer.tsx
+++ src/home/footer/footer.tsx
@@ -8,9 +8,9 @@ export default function Footer (){
             <h2>FYZZTECH©</h2>
             <p>fyzztech@gmail.com</p>
             <div>
-                <a href=""> <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn" id="Linkedin"/> </a>
-                <a href=""> <img src="https://i.postimg.cc/Jh94rRn7/Logo-Insta.png" alt="Instagram" /> </a>
-                <a href=""> <img src="https://i.postimg.cc/sxzjWK7t/logo-Tiktok.jpg" alt="Tiktok" /> </a>
+                <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn" id="Linkedin"/> 
+                <img src="https://i.postimg.cc/Jh94rRn7/Logo-Insta.png" alt="Instagram" /> 
+                <img src="https://i.postimg.cc/sxzjWK7t/logo-Tiktok.jpg" alt="Tiktok" /> 
             </div>
             <img src="https://i.postimg.cc/sDwjBYXN/Photo-Next.png" alt="Site Feito em Next.js" />
         </footer>
