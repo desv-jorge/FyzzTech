f1eea6b9da1300ee54338d6370f846a09c7f5760
JORGE <jorgenathanael53@gmail.com>
2025-02-16 22:40:32 -0300

    configuring server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+require('dotenv').config();
 import cors from "@fastify/cors";
 import Fastify from "fastify";
 import { routes } from "./routes";
 
 const app = Fastify({logger : true})
+const port = 4000;
+
 
 app.setErrorHandler((error,request,reply)=>{
     reply.code(400).send({ message: error.message })
@@ -12,9 +15,10 @@ const start = async ()=>{
 
     await app.register(cors)
     await app.register(routes)
+    const porta = port;
 
     try{
-        await app.listen({ port : 3001})
+        await app.listen({ port : porta})
     }catch(error){
         process.exit(1)
     }
